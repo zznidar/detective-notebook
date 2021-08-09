cccf562136a30bff1c9b9bc157926ea96f46fffd
zznidar <zznidar@users.noreply.github.com>
2021-08-09 22:28:51 +0200

    Add clues fn, order generator

--- runtime.js
+++ runtime.js
@@ -24,6 +24,17 @@ class Player {
         this.holds = new Cards();
         this.showed = [];
     }
+
+    clues() {
+        let clues = [];
+        // We know for each round the cards called.
+        // Combine this knowledge wih lacking cards to determine possible holds.
+        for(let t of this.showed) {
+            // Find possible holds for each round
+            clues.push(t.filter((el) => { return !this.lacks[el] }));
+        }
+        return(clues);
+    }
 }
 
 class Round {
@@ -38,20 +49,28 @@ class Round {
 }
 
 function* order() {
+    //let playersOrder = players.keys();
+    let NPlayers = playersOrder.length;
+    let current = 0;
+
     while(true) {
-        let 
+        console.log("plO : ", playersOrder);
+        let jump = yield playersOrder[current];
+        if(!isNaN(jump)) current = jump;
+        current = (++current)%NPlayers;
+        console.log(current);
     }
 }
 
 rounds = []; // Array of all rounds
 const players = new Map(); // Array of players added in order, starting with yourself
 
+playersOrder = ["Me", "Player1", "Player2", "Player3", "Player4", "Player5"];
+for(let playername of playersOrder) {
+    players.set(playername, new Player());
+}
 
-players.set("Me", new Player());
-players.set("Player1", new Player());
-players.set("Player2", new Player());
-players.set("Player3", new Player());
-players.set("Player4", new Player());
-players.set("Player5", new Player());
-
-
+// test of clues
+players.get("Me").showed.push(["plum", "green", "white"])
+players.get("Me").lacks["plum"] = true
+players.get("Me").clues()
